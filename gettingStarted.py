44011ea0685b0218f0e2e9b22a936a7403366702
paoboyd <po2156@nyu.edu>
2025-02-01 00:21:12 -0500

    Update gettingStarted.py

--- gettingStarted.py
+++ gettingStarted.py
@@ -20,13 +20,13 @@ def welcome_assignment_answers(question):
     elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
         answer = "No"
     elif question == "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
-        answer = 3
+        answer = 4
     elif question == "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
         answer = 3
 
     else: 
         ### you should understand why this else case should be included
-        answer = "Error"
+        answer = "This is not my beautiful wife! This is not my beautiful car! How did I get here?"
     return(answer)
 # Complete all the questions
 
